420468f658b7acfb2e3e94a907b2363fa1e6f41d
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-12 14:20:25 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ provider "kubectl" {
   host  = "https://${data.google_container_cluster.primary.endpoint}"  
   token                  = data.google_client_config.default.access_token    
   cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  load_config_file       = false
   # client_key             = base64decode(data.google_container_cluster.primary.master_auth.0.client_key)
   # client_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.client_certificate)
 }
