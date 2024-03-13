08043a4815dd3c8649574dd09b06f925e855d222
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-13 17:44:44 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,15 +9,4 @@ output "project" {
   value = data.google_client_config.default
   sensitive = true
 }
-output "datacluster" {
-  value       = data.google_container_cluster.primary
-}
-/*output "gke_auth" {
-  value = module.gke_auth
-  sensitive = true
-}
 
-output "content" {
-  value = kubernetes_manifest.cluster_issuer.manifest
-}
-*/
