6f6fcd11b004446bfb6b52d8984ecaea89d0ebd4
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-15 16:00:26 +0800

    Update main.tf

--- modules/check-namespace/main.tf
+++ modules/check-namespace/main.tf
@@ -1,11 +1,11 @@
 data "google_client_config" "provider" {}
 
-data "google_container_cluster" "my_cluster" {
+data "google_container_cluster" "primary" {
   name     = "fyp-vpc-cluster"
   location = "us-central1"
 }
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  host  = "https://${data.google_container_cluster.primary.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
